6bfbe7d1ecbf57e1cc427a22b1bb9733e92e4109
maeflor <maeflor.butawan@bisu.edu.ph>
2023-11-30 14:13:23 +0800

    2nd commit

---
+++ information.js
@@ -0,0 +1,66 @@
+export const datas = [
+    {
+      nickName:'Buts',
+      name: {
+        firstName:'Mae Flor',
+        lastName:'Butawan',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickName:'Ave',
+      name: {
+        firstName:'Avery',
+        lastName:'Butawan',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+    {
+      nickName:'Jojow',
+      name: {
+        firstName:'MARJO',
+        lastName:'Gideon',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+    {
+      nickName:'Caloy',
+      name: {
+        firstName:'Carlo',
+        lastName:'Namor',
+      },
+      course:'BSIT',
+      year:'<3',
+    },
+    {
+      nickName:'Lyn',
+      name: {
+        firstName:'Jeeve',
+        lastName:'Lunzon',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+    {
+      nickName:'Nikkos',
+      name: {
+        firstName:'Nikko',
+        lastName:'Egama',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+    {
+      nickName:'Bossing',
+      name: {
+        firstName:'Rea',
+        lastName:'Patac',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+  ];
